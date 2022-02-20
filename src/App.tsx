e6b64598a5608cb470d817dd8666861a46959a4a
| Ali soleymani | <alisoleimani199@gmail.com>
2022-02-20 13:07:02 +0330

    Create fetch custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import Header from "./components/Header"
+import { useCallback, useEffect, useMemo } from 'react'
+
+import Header from './components/Header'
+import useFetch from './hooks/useFetch'
 
 import './index.css'
 
 function App() {
+  const [word] = useFetch('http://localhost:3001/word')
+
+  useEffect(() => {
+    console.log(word)
+  }, [word])
+
   return (
     <main className="w-full">
       <Header />
